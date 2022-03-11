3a55af0151b563607e589c81327fed9aeab53054
phamhuynh161099 <phamhuynh161099@gmail.com>
2022-03-11 11:18:43 +0700

    add say hi methdo

--- Dog.js
+++ Dog.js
@@ -1,7 +1,14 @@
-function Dog(){
+const { Console } = require("console");
+
+function Dog(name){
     this.stomach=[];
+    this.name=name
 }
 
 Dog.prototype.eat=function(cat){
     this.stomach.push(cat);
+}
+
+Dog.prototype.sayHi=function(){
+    Console.log('Say Hi')
 }
